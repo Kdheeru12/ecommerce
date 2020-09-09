79480c7dbdcad7e35a9fa2aa6910a5e65f139899
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2020-09-09 13:31:10 +0530

    anyo

--- shop/models.py
+++ shop/models.py
@@ -45,6 +45,8 @@ class OrderItem(models.Model):
     order=models.ForeignKey(Order,on_delete=models.SET_NULL,blank=True,null=True)
     quantity = models.IntegerField(default=0,null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return str(self.order.id)
     @property
     def get_total(self):
         total = self.product.price * self.quantity
--- static/js/cart.js
+++ static/js/cart.js
@@ -1,4 +1,21 @@
-console.log('hello world')
+function getCookie(name){
+    var cookieArr = document.cookie.split(";");
+    for(var i=0;i<cookieArr.length;i++){
+        var cookiePair = cookieArr[i].split("=");
+        if(name == cookiePair[0].trim()){
+            return decodeURIComponent(cookiePair[1])
+        }
+    }
+    return null
+}
+var cart = JSON.parse(getCookie('cart'))
+if(cart == undefined){
+    cart = {}
+    console.log('cart created')
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+}
+console.log('Cart:',cart)
+console.log('hello worlds')
 var updateBtns = document.getElementsByClassName('update-cart')
 function getToken(name) {
     var cookieValue = null;
@@ -23,13 +40,37 @@ for (i=0;i<updateBtns.length;i++){
         var action = this.dataset.action
         console.log('productId:',productId,'Action:',action)
         if (user == 'AnonymousUser'){
-            console.log('not aut')
+            addCookieItem(productId,action)
         }
         else{
             updateUserOrder(productId,action)
             
         }
     })
+}
+function addCookieItem(productId,action){
+    console.log('sss')
+    if (action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+            console.log('a')
+        }
+        else{
+            cart[productId]['quantity'] += 1
+            console.log('v')
+        }
+    }
+    if (action == 'remove'){
+        cart[productId]['quantity'] -= 1 
+        if(cart[productId]['quantity'] <= 0){
+            console.log('remove')
+            delete cart[productIds]
+        }
+    }
+    console.log('Cart:',cart)
+
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 function updateUserOrder(productId,action){
     console.log('json is workings')
